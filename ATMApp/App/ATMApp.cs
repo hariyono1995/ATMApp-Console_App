d9b6d0d0a4a3b51b11801b5864b4811cdffe30d9
hariyono <96717662+hariyono1995@users.noreply.github.com>
2022-09-13 17:31:12 +0700

    Add project files.

---
+++ ATMApp/App/ATMApp.cs
@@ -0,0 +1,18 @@
+﻿using ATMApp.UI;
+using System;
+
+namespace ATMApp.App
+{
+    internal class ATMApp
+    {
+        static void Main(string[] args)
+        {
+            AppScreen.Welcome();
+
+            long cardNumber = Validator.Convert<long>("your card number");
+            Console.WriteLine($"Your card number is {cardNumber}");
+
+            Utility.PressEnterToContinue();
+        }
+    }
+}
---
+++ ATMApp/UI/AppScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            Console.Clear();
+            Console.Title = "ATM Money App";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\n---------------Welcome to ATM Money---------------\n\n");
+
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate a physical ATM card. read the card number and validate it.\n\n");
+
+            Utility.PressEnterToContinue();
+        }
+
+
+    }
+}
